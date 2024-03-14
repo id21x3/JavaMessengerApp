313ff4983117227598f6e68eb0dfc7419b08d25f
Ihor Danchuk <ihor.danchuk@student.tuke.sk>
2024-03-14 13:05:30 +0100

    Project creation

---
+++ src/main/java/com/msg/JavaMessengerAppApplication.java
@@ -0,0 +1,13 @@
+package com.msg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMessengerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMessengerAppApplication.class, args);
+	}
+
+}
